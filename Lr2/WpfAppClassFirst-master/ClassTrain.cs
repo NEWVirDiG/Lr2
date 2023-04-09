ada1de7b1ef241f4944fa2acf58058dd55cdbbd7
NEWVirDiG <128370298+NEWVirDiG@users.noreply.github.com>
2023-04-09 22:15:25 +0300

    Add files via upload

---
+++ Lr2/WpfAppClassFirst-master/ClassTrain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOOPClasses //пространство имен проекта
+    {
+        internal class ClassTrain
+        {
+            //поля
+            private string punktNasnachenia;
+            private DateTime Dstart;
+            private string nTrain;
+            // свойства для доступа к полям
+            public string PunktNasnachenia
+            {
+                get { return punktNasnachenia; }//возвращает значение поля
+                set { punktNasnachenia = value; } //устанавливает значение поля
+            }
+            public DateTime DStart
+            {
+                get { return Dstart; }
+                set { Dstart = value; }
+            }
+            public string ntrain
+            {
+                get { return nTrain; } //возвращает значение поля
+                set { nTrain = value; } //устанавливает значение поля
+            }
+            //методы 
+            //конструктор без параметров задает значения полей по умолчанию
+            public ClassTrain()
+            {
+                punktNasnachenia = "Куровское";
+                nTrain = "22";
+                Dstart = new DateTime(2023, 02, 01);
+            }
+            //конструктор с параметрами
+            public ClassTrain(string f, DateTime d, string ad,
+                string ph, byte k,
+                string g, double s)
+            {
+                punktNasnachenia = f;
+                Dstart = d;
+                nTrain = ad;
+
+            }
+            //метод вывода на экран
+            public string PrintToScreen()
+            {
+                string line = $"Пункт назначения {punktNasnachenia} дата отпрвления {Dstart}" + "\n";
+                line = ($"Номер поезда {nTrain}");
+                return line;
+            }
+            public void SaveToFile(string filename)
+            {
+                StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+                streamWriter.WriteLine("Данные о работе поезда");
+                streamWriter.WriteLine($"Пункт назначения: {punktNasnachenia}");
+                streamWriter.WriteLine($"Номер поезда: {nTrain}");
+                streamWriter.WriteLine($"Дата поступления: {Dstart.ToString()}");
+                streamWriter.Close();
+
+            }
+            //метод ввода с клавиатуры
+        }
+    }
---
+++ Lr2/WpfAppClassFirst-master/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleAppOOPClasses;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppClassFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassTrain train = new ClassTrain();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSavestudent_Click(object sender, RoutedEventArgs e)
+        {
+            train.ntrain = TxtN.Text;
+            train.DStart = (DateTime)DtDstart.SelectedDate;
+            train.PunktNasnachenia = TxtX.Text; ;
+
+            LstTrain.Items.Clear();
+            LstTrain.Items.Add(train.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            train.SaveToFile(saveFileDialog.FileName);
+        }
+    }
+}
